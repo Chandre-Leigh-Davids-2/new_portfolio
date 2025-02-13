b47469fcf15c88aef770d735c13e416a20840830
Chandre-Leigh-Davids-2 <chandreleighdavids.17@gmail.com>
2025-02-13 11:06:42 +0200

    edit

--- src/components/About.vue
+++ src/components/About.vue
@@ -31,7 +31,8 @@
                   <ul>
                     <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>04 October 2005</span></li>
                     <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span><a
-                          href="https://portfoliocldavids.vercel.app/" target="_blank">https://portfoliocldavids.vercel.app/</a></span></li>
+                          href="https://portfoliocldavids.vercel.app/"
+                          target="_blank">https://portfoliocldavids.vercel.app/</a></span></li>
                     <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+27 73 325 3129</span></li>
                     <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Cape Town. South Africa</span>
                     </li>
@@ -43,7 +44,8 @@
                     <li><i class="bi bi-chevron-right"></i> <strong>Credentials:</strong> <span>Certificate of
                         Completion </span></li>
                     <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong>
-                      <span>chandreleighdavids.17@gmail.com</span></li>
+                      <span>chandreleighdavids.17@gmail.com</span>
+                    </li>
                     <li><i class="bi bi-chevron-right"></i> <strong>Status:</strong> <span>Open To Work</span></li>
                   </ul>
                 </div>
@@ -68,21 +70,41 @@
         <!-- Section Title -->
         <div class="container section-title" data-aos="fade-up">
           <h2>Technical Skills</h2>
-          <p>My technical skills include proficiency in HTML, CSS, JavaScript, and basic knowledge of frameworks like React. I'm always eager to learn more and expand my skill set, whether it's diving deeper into backend development or exploring new tools and technologies to enhance my projects.</p>
+          <p>My technical skills include proficiency in HTML, CSS, JavaScript, and basic knowledge of frameworks like
+            React. I'm always eager to learn more and expand my skill set, whether it's diving deeper into backend
+            development or exploring new tools and technologies to enhance my projects.</p>
         </div><!-- End Section Title -->
-        <div class="container" data-aos="fade-up" data-aos-delay="100">
-          
+        <div class="row d-flex justify-content-center x" data-aos="fade-up" data-aos-delay="100">
+          <div class="skill col-sm-2"><img src="../assets//images/html-5.png" alt=""></div>
+          <div class="skill col-sm-2"><img src="../assets//images/css.png" alt=""></div>
+          <div class="skill col-sm-2"><img src="../assets//images/github.png" alt=""></div>
+          <div class="skill col-sm-2"><img src="../assets//images/vuejs.png" alt=""></div>
+          <div class="skill col-sm-2"><img src="../assets//images/node.png" alt=""></div>
+          <div class="skill col-sm-2"><img src="../assets//images/mySQL.png" alt=""></div>
+          <div class="skill col-sm-2"><img src="../assets//images/wordpressIcon.png" alt=""></div>
+          <div class="skill col-sm-2"><img src="../assets//images/seo-search-symbol.png" alt=""></div>
+          <div class="skill col-sm-2"><img src="../assets//images/pythonIcon.png" alt=""></div>
+          <div class="skill col-sm-2"><img src="../assets//images/c-.png" alt=""></div>
         </div>
         <div class="container section-title" data-aos="fade-up">
           <h2>Soft Skills</h2>
-          <p>I excel in communication, problem-solving, and teamwork. I’m adaptable and thrive in collaborative environments, always open to feedback and eager to improve. I’m committed to fostering positive relationships and ensuring tasks are completed efficiently while maintaining a strong attention to detail.</p>
+          <p>I excel in communication, problem-solving, and teamwork. I’m adaptable and thrive in collaborative
+            environments, always open to feedback and eager to improve. I’m committed to fostering positive
+            relationships and ensuring tasks are completed efficiently while maintaining a strong attention to detail.
+          </p>
+          <div class="container-fluid" data-aos="fade-up" data-aos-delay="100">
+          <ul>
+            <li><i class="bi bi-chevron-right"></i>Disciplined</li>
+            <li><i class="bi bi-chevron-right"></i>Orderly</li>
+            <li><i class="bi bi-chevron-right"></i>Dedicated</li>
+            <li><i class="bi bi-chevron-right"></i>Collaborative</li>
+            <li><i class="bi bi-chevron-right"></i>Adaptable</li>
+            <li><i class="bi bi-chevron-right"></i>Inquisitive</li>
+          </ul>
+        </div>
         </div><!-- End Section Title -->
 
-        <div class="container" data-aos="fade-up" data-aos-delay="100">
-
-
-
-        </div>
+        
 
       </section><!-- /Skills Section -->
 
@@ -99,6 +121,7 @@ export default {
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
 .img {
   width: 80%;
 }
@@ -112,16 +135,34 @@ img {
   min-height: 100vh;
   border: 4px solid #E5B80B;
 }
-ul li{
+
+ul li {
   list-style: none;
 }
+
 i {
   color: pink;
 }
-h2{
+
+h2 {
   font-family: "Fredoka", serif;
 }
-p{
+
+p {
   font-family: "Poppins", serif;
+}
+
+.x {
+  padding: 5% 12% 5% 12%;
+}
+
+.skill {
+  width: 9vw;
+}
+
+@media screen and (max-width:645px) {
+  .skill {
+    width: 15vw;
+  }
 }
 </style>
